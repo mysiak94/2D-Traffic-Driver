e84e76b2b7a36880548c92ed722354c527184f42
mysiak94 <mysiak1994@gmail.com>
2016-03-31 13:20:24 +0200

    First commit

---
+++ Assets/scripts/carControls.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carControls : MonoBehaviour {
+	
+	public float carSpeed;
+
+	public float maxPos = 2.2f;
+	Vector2 position;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+
+		position.x = Mathf.Clamp (position.x, -2.0f, 2.2f);
+
+		transform.position = position;
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "Enemy Car") {
+			Destroy (gameObject);
+		}
+
+	}
+
+}
---
+++ Assets/scripts/carSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carSpawner : MonoBehaviour {
+
+	public GameObject car;
+	public float maxPos = 2.2f;
+	public float delayTimer = 0.5f;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+		timer = delayTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			Vector3 carPos = new Vector3 (Random.Range (-2.0f, 2.2f), transform.position.y, transform.position.z);
+
+			Instantiate (car, carPos, transform.rotation);
+			timer = delayTimer;
+		}
+	}
+}
